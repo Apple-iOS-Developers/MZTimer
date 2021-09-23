d1cbbc5e97e0e02d6a3523c10e5fa4c2551423f7
TaeHyeong Kim <uuzaza@naver.com>
2021-09-24 00:30:06 +0900

    view -> image util code

--- MZTimer/Helper/InstagramShareHelper.swift
+++ MZTimer/Helper/InstagramShareHelper.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 class InstagramShareHelper {
     static func shareToInstagram(event: Event) {
@@ -48,5 +49,22 @@ class InstagramShareHelper {
         return renderer.image(actions: { context in
             text.draw(at: CGPoint.zero, withAttributes: attributes)
         })
+    }
+}
+// 추후 인스타 공유시 view -> image util code
+extension View {
+    func snapshot() -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        let view = controller.view
+
+        let targetSize = controller.view.intrinsicContentSize
+        view?.bounds = CGRect(origin: .zero, size: targetSize)
+        view?.backgroundColor = .clear
+
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+
+        return renderer.image { _ in
+            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+        }
     }
 }
