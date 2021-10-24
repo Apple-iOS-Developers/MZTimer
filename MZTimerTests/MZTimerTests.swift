c7afb776664afbb604231dc73157feda5f367b0a
김태형[Music iOS Dev] <uuzaza@naver.com>
2021-10-24 21:09:36 +0900

    add sample test

---
+++ MZTimerTests/MZTimerTests.swift
@@ -0,0 +1,33 @@
+//
+//  MZTimerTests.swift
+//  MZTimerTests
+//
+//  Created by TaeHyeongKim on 2021/10/24.
+//
+
+import XCTest
+
+class MZTimerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(false, true)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
